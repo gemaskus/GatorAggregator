838942cbe6b2145df3f69e82bebab9fad1cb1e38
Grant Maskus <gemaskus@icloud.com>
2024-11-13 16:24:27 -0700

    Blog aggregator ch2 lesson3

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/gemaskus/GatorAggregator/internal/config"
+	"github.com/gemaskus/GatorAggregator/internal/database"
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db            database.Queries
 	currentConfig *config.Config
 }
 
@@ -26,7 +33,15 @@ func main() {
 	if err != nil {
 		log.Fatalf("error reading the config file: %v", err)
 	}
+
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatalf("Failed to open the postgres database: %v", err)
+	}
+
+	dbQueries := database.New(db)
 	currentState := state{
+		db:            *dbQueries,
 		currentConfig: &cfg,
 	}
 
@@ -34,6 +49,7 @@ func main() {
 		handlers: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	args := os.Args
 
@@ -47,7 +63,7 @@ func main() {
 
 	err = cmds.run(&currentState, cmd)
 	if err != nil {
-		log.Fatal("A username is required")
+		log.Fatalf("%v", err)
 	}
 }
 
@@ -55,12 +71,46 @@ func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
 		return fmt.Errorf("Login requires only one argument")
 	}
-	err := s.currentConfig.SetUser(cmd.args[0])
+
+	user, err := s.db.GetUser(context.Background(), cmd.args[0])
+
+	if err != nil {
+		return fmt.Errorf("User does not exist in database: %v", err)
+	}
+
+	err = s.currentConfig.SetUser(user.Name)
 	if err != nil {
 		return err
 	}
 
 	fmt.Printf("Username has been set: %v\n", s.currentConfig.CurrentUserName)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("Register requires only one argument")
+	}
+
+	if _, err := s.db.GetUser(context.Background(), cmd.args[0]); err == nil {
+		return fmt.Errorf("User already exists")
+	}
+
+	newUserParams := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().Local(),
+		UpdatedAt: time.Now().Local(),
+		Name:      cmd.args[0],
+	}
+
+	newUser, err := s.db.CreateUser(context.Background(), newUserParams)
+	if err != nil {
+		return err
+	}
+
+	err = s.currentConfig.SetUser(newUser.Name)
+	fmt.Printf("New User created: %v\n", newUser)
+
 	return nil
 }
 
